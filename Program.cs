e3de6539d1db1c67f92fa21c7a27bab665ddfe0c
Yukiel <kevinyukielcoronaramos@gmail.com>
2026-01-14 13:43:42 -0600

    Subiendo nuevas funciones basicas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CSharpHelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // Hola mundo
+            Console.WriteLine("Hello, C#");
+
+            /*
+                Comentario de
+                multiples lineas
+            */
+
+            string myString = "Este es un string";
+            Console.WriteLine(myString);
+
+            myString = "Este es una valir de cambio de la cadena";
+            Console.WriteLine(myString);
+
+            int myInt = 6;
+            myInt = myInt + 4;
+            Console.WriteLine(myInt);
+            Console.WriteLine(myInt+5);
+
+            double myDouble =6.5;
+            Console.WriteLine(myDouble);
+
+            float myFloat = 6.5f;
+            Console.WriteLine(myFloat);
+
+        }
+    }
+}
+
+
+
