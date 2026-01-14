9c38316280d9a8ac7289fe2c527a8cb5ec7ce3aa
Yukiel <140352744+YukielCr@users.noreply.github.com>
2026-01-14 17:52:09 -0600

    Uso de bucles

--- Program.cs
+++ Program.cs
@@ -31,6 +31,53 @@ namespace CSharpHelloWorld
             float myFloat = 6.5f;
             Console.WriteLine(myFloat);
 
+            bool myBool=true;
+            myBool=false;
+            Console.WriteLine(myBool);
+
+
+            Console.WriteLine($"El valor de mi entero {myInt} es y valor de mi bool {myBool}");
+
+
+            const string MyConst="Mi constante";
+            Console.WriteLine(MyConst);
+
+            // Estructuras de Array
+
+            var myArray= new String[] {"Yukiel","Kevin","Codere"};
+            Console.WriteLine(myArray[2]);
+
+            myArray[2]="36";
+            Console.WriteLine(myArray[2]);
+
+            // Uso de los diccionarios
+            var myDictionary = new Dictionary<string, int>
+            {
+                {"Yukiel", 21},
+                {"Arleth", 23}
+            };
+
+            Console.WriteLine(myDictionary["Arleth"]);
+
+            // Esto es una lista estas no se repiten
+            var mySet=new HashSet<string>{"Yukiel","Kevin","Codere","Yukiel"};
+            Console.WriteLine(mySet);
+
+
+            // El usa de las tuplas 
+            var myTuple =("Yukiel","Kevin","Codere","Yukiel");
+            Console.WriteLine(myTuple);
+
+            // Bucles
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            
+            
         }
     }
 }
